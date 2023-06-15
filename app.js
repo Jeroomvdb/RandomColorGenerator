75f907bf568da34b88aeae9e9752b1cce30a4321
Jeroomvdb <125352201+Jeroomvdb@users.noreply.github.com>
2023-06-15 13:14:12 +0200

    with atilla fix

--- app.js
+++ app.js
@@ -144,66 +144,75 @@ document.addEventListener('keydown', function (event) {
 
 
 
-// Update 'your colors'
+// // Update 'your colors' -> this works / with override of storage methods (see below for explanation)
 
-function handleLocalStorageChange() {
-  const colorImagesContainer = document.getElementById('colorImages');
+// function handleLocalStorageChange() {
+//   const colorImagesContainer = document.getElementById('colorImages');
 
-  // Clear existing <div> elements
-  while (colorImagesContainer.firstChild) {
-    colorImagesContainer.firstChild.remove();
-  }
+//   // Clear existing <div> elements
+//   while (colorImagesContainer.firstChild) {
+//     colorImagesContainer.firstChild.remove();
+//   }
 
-  // Loop through localStorage key-value pairs and create <div> elements
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const colorValue = localStorage.getItem(key);
+//   // Loop through localStorage key-value pairs and create <div> elements
+//   for (let i = 0; i < localStorage.length; i++) {
+//     const key = localStorage.key(i);
+//     const colorValue = localStorage.getItem(key);
 
-    // Create a new <div> element
-    const newDiv = document.createElement('div');
-    newDiv.style.width = '50px';
-    newDiv.style.height = '50px';
-    newDiv.style.background = colorValue;
-    newDiv.className='col s4'
+//     // Create a new <div> element
+//     const newDiv = document.createElement('div');
+//     newDiv.style.width = '50px';
+//     newDiv.style.height = '50px';
+//     newDiv.style.background = colorValue;
+//     newDiv.className='col s4'
 
-    // Append the new <div> to the container
-    colorImagesContainer.appendChild(newDiv);
+//     // Append the new <div> to the container
+//     colorImagesContainer.appendChild(newDiv);
+//   }
+// }
+
+// // Override the localStorage methods --- explanation below why
+
+// // The code you provided should work correctly and log a message to the console when an item is added to the localStorage. However, there are a couple of things you should keep in mind:
+// // The storage event is only triggered when changes are made to the localStorage from a different tab or window. It won't be triggered in the same tab or window where the changes are made. So, if you're testing the code by adding an item to the localStorage in the same tab or window where the code is running, the event won't be fired in that tab or window.
+// // Make sure you're using a different origin (domain, protocol, and port) or different tabs/windows of the same origin to test the code. If you're testing the code in an environment where the localStorage is shared across different tabs or windows with the same origin, the event should be triggered correctly.
+// // To test the code properly, open a new tab or a new window with the same origin and add an item to the localStorage there. Then switch to the tab or window where the code is running, and you should see the console.log statement executed.
+// // If the code still doesn't log anything, please provide more details about the environment you are running the code in, the steps you're taking to test it, and any error messages you're encountering.
+
+
+// const originalSetItem = localStorage.setItem;
+// localStorage.setItem = function(key, value) {
+//   originalSetItem.apply(this, arguments);
+//   handleLocalStorageChange();
+// };
+
+// const originalRemoveItem = localStorage.removeItem;
+// localStorage.removeItem = function(key) {
+//   originalRemoveItem.apply(this, arguments);
+//   handleLocalStorageChange();
+// };
+
+// window.addEventListener('storage', handleLocalStorageChange);
+// window.addEventListener('load', handleLocalStorageChange)
+
+
+
+// ATILLA CODE -> works on load / not on storage
+function updateYourColors(){
+  let colors = []
+
+  document.getElementById("colorImages").innerHTML = "";
+
+
+  for(let i=0;i<localStorage.length;i++){
+    let keyName = localStorage.key(i)
+
+    document.getElementById("colorImages").innerHTML +=`<div style="width: 50px; height: 50px; background:${keyName};"></div>`
   }
 }
 
-// Override the localStorage methods --- explanation below why
-
-// The code you provided should work correctly and log a message to the console when an item is added to the localStorage. However, there are a couple of things you should keep in mind:
-// The storage event is only triggered when changes are made to the localStorage from a different tab or window. It won't be triggered in the same tab or window where the changes are made. So, if you're testing the code by adding an item to the localStorage in the same tab or window where the code is running, the event won't be fired in that tab or window.
-// Make sure you're using a different origin (domain, protocol, and port) or different tabs/windows of the same origin to test the code. If you're testing the code in an environment where the localStorage is shared across different tabs or windows with the same origin, the event should be triggered correctly.
-// To test the code properly, open a new tab or a new window with the same origin and add an item to the localStorage there. Then switch to the tab or window where the code is running, and you should see the console.log statement executed.
-// If the code still doesn't log anything, please provide more details about the environment you are running the code in, the steps you're taking to test it, and any error messages you're encountering.
-
-
-const originalSetItem = localStorage.setItem;
-localStorage.setItem = function(key, value) {
-  originalSetItem.apply(this, arguments);
-  handleLocalStorageChange();
-};
-
-const originalRemoveItem = localStorage.removeItem;
-localStorage.removeItem = function(key) {
-  originalRemoveItem.apply(this, arguments);
-  handleLocalStorageChange();
-};
-
-window.addEventListener('storage', handleLocalStorageChange);
-window.addEventListener('load', handleLocalStorageChange)
-
-
-
-
-
-
-
-
-
-
+window.addEventListener('storage', updateYourColors);
+window.addEventListener('load', updateYourColors);
 
 
 
